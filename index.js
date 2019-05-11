0218f5c21574317ce0ce36027bbd8d28dfae29ae
Beyonce Knowles <beyonce@thinkful.com>
2019-05-11 17:09:39 -0500

    First final submit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 function classToggle() {
-    const navs = document.querySelectorAll('.drop-down-nav')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+    $('.Navbar__Link-toggle').click(function() {
+        $('.drop-down-nav').toggleClass('Navbar__ToggleShow');
+    });
+}
+
+classToggle();
